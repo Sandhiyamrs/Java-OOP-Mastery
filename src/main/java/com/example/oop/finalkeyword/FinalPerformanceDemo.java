9b83894dd81683eaeeac154af64d5da9e7e6e925
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-31 22:36:32 +0530

    Create FinalPerformanceDemo.java

---
+++ src/main/java/com/example/oop/finalkeyword/FinalPerformanceDemo.java
@@ -0,0 +1,9 @@
+package com.example.oop.finalkeyword;
+
+public class FinalPerformanceDemo {
+    public static void main(String[] args) {
+
+        final int x = 10;
+        System.out.println("Final value: " + x);
+    }
+}
