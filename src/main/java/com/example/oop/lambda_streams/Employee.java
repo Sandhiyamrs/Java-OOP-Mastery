e80c326b45e36fe6da102336c62fa37ebd5eeb79
Sandhiya M <157009459+Sandhiyamrs@users.noreply.github.com>
2025-12-31 22:13:23 +0530

    Create Employee.java

---
+++ src/main/java/com/example/oop/lambda_streams/Employee.java
@@ -0,0 +1,14 @@
+package com.example.oop.lambda_streams;
+
+public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() { return name; }
+    public double getSalary() { return salary; }
+}
